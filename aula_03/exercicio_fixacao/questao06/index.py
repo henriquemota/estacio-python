75ecad0036fa8aa9894b543f99dd2a07d6239829
Henrique Nogueira da Gama Mota <mota.henrique@gmail.com>
2021-04-05 07:56:31 -0300

    exercicio 06 manipulacao de arquivos

--- aula_03/exercicio_fixacao/questao06/index.py
+++ aula_03/exercicio_fixacao/questao06/index.py
@@ -1,10 +1,28 @@
-# Escreva uma função que recebe dois nomes de arquivos e 
-# copia o conteúdo do primeiro arquivo para o segundo arquivo. 
-# Considere que o conteúdo do arquivo de origem é um texto. 
+# Escreva uma função que recebe dois nomes de arquivos e
+# copia o conteúdo do primeiro arquivo para o segundo arquivo.
+# Considere que o conteúdo do arquivo de origem é um texto.
 # Sua função não deve copiar linhas comentadas (que começão com //).
 import os
 from os import path
 
 BASE_DIR = path.dirname(path.abspath(__file__))
 
-def mymerge(ori, des):
+
+def my_merge():
+    ori = input('informe o nome do arquivo de origem: ')
+    des = input('informe o nome do arquivo de destino: ')
+
+    ORI_FILE = path.join(BASE_DIR, ori)
+    DES_FILE = path.join(BASE_DIR, des)
+
+    with open(ORI_FILE, 'r') as file_read:
+        linhas = file_read.readlines()
+        with open(DES_FILE, 'a') as file_write:
+            for linha in linhas:
+                if ('//' not in linha):
+                    file_write.write(linha)
+
+    print('final de processamento')
+
+
+my_merge()
